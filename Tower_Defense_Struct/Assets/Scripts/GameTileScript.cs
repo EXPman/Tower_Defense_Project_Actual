b5a71755f5d3d04f1f4d660b8a7690a19bbac0ba
Furizax <148867645+Furizax@users.noreply.github.com>
2024-04-11 19:50:42 -0400

    PathFinding

--- Tower_Defense_Struct/Assets/Scripts/GameTileScript.cs
+++ Tower_Defense_Struct/Assets/Scripts/GameTileScript.cs
@@ -6,22 +6,33 @@ using UnityEngine.EventSystems;
 public class GameTileScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     private SpriteRenderer spriteRenderer;
+    private Color originalColor;
+
+
     [SerializeField] SpriteRenderer HoverRenderer;
     [SerializeField] SpriteRenderer SpawnerRenderer;
+
+    public GameManagerScript GM { get; internal set; }
+    public int X { get; internal set; }
+    public int Y { get; internal set; }
+    public bool IsBlocked { get; private set; }
 
     private void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.color;
     }
 
     internal void TurnGrey()
     {
         spriteRenderer.color = Color.gray;
+        originalColor = spriteRenderer.color; 
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         HoverRenderer.enabled = true;
+        GM.TargetTile = this;
         Debug.Log("enter");
     }
 
@@ -31,15 +42,25 @@ public class GameTileScript : MonoBehaviour, IPointerEnterHandler, IPointerExitH
         Debug.Log("exit");
     }
 
+    public void onPointerDown(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Click");
+
+    }
 
     internal void SetEnemySpawn()
     {
         SpawnerRenderer.enabled = true;
 
     }
-    public void OnPointerClick(PointerEventData eventData)
+    internal void SetPath(bool isPath)
     {
-        Debug.Log("Click");
-
+      spriteRenderer.color = isPath ? Color.green : originalColor;
     }
+   
 }
